5d7b421461695010d034e42ac804dce98845d749
새로운 사용자 이름 <  >
2024-11-19 14:00:07 +0900

    first

---
+++ a1118/KeyCode.java
@@ -0,0 +1,15 @@
+package a1118;
+
+import java.io.IOException;
+
+public class KeyCode {
+    public static void main(String[] args) throws IOException{
+        int keyCode;
+        keyCode = System.in.read();
+        System.out.println(keyCode);
+        keyCode = System.in.read();
+        System.out.println(keyCode);
+        keyCode = System.in.read();
+        System.out.println(keyCode);
+    }
+}
---
+++ a1118/StirngSample.java
@@ -0,0 +1,13 @@
+package a1118;
+
+public class StirngSample {
+
+    public static void main(String[] args) {
+        char ch = 'A';
+        System.out.println(ch);
+        char [] hello = {'H','e','l','l','o'};
+        System.out.println(hello);
+        String strHello = "Hello";
+        System.out.println(strHello+"World");
+    }
+}
